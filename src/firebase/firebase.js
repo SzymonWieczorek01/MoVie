d8b835fb76d234ae5d9eb1be40cc68caa53bb387
stepsofi <s217594@uek.krakow.pl>
2024-05-01 18:25:39 +0200

    firebase start

---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDeJo9HLNkPDN7iRWLNWKcKHnsJ2uuyLek",
+    authDomain: "movie-cfb9d.firebaseapp.com",
+    projectId: "movie-cfb9d",
+    storageBucket: "movie-cfb9d.appspot.com",
+    messagingSenderId: "411708747738",
+    appId: "1:411708747738:web:6f05eeab599a0e294bdae3",
+    measurementId: "G-G15EZZHEH0"
+  };
+  
+  
+  const db = initializeApp(firebaseConfig);
+  
--- src/main.js
+++ src/main.js
@@ -4,5 +4,8 @@ import router from '../src/router/router'
 import Vue3TouchEvents from 'vue3-touch-events'
 import store from '../src/store/store';
 import YouTube from 'vue3-youtube'
+import app from '../src/firebase/firebase'
+
 
 createApp(App).use(Vue3TouchEvents).use(store).use(router).component('YouTube', YouTube).mount('#app')
+
