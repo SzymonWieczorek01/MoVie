9561c32202f0927d87f3920facb292097b54ee22
Volodymyr <volodymyr.brintsov@here.com>
2024-05-13 23:43:21 +0200

    Added slight modification

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -117,7 +117,7 @@ export default {
   align-items: center;
   padding: 0;
   margin: 0;
-  background: #cfcfcf;
+  background: white;
   padding: 10px 20px;
 }
 
@@ -127,7 +127,7 @@ export default {
 
 .navbar .primary-navigation li a {
   text-decoration: none;
-  color: #ffffff;
+  color: white;
   font-weight: 600;
   transition: color 1s ease ;
 }
